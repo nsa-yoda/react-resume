64052055d148145674139808ba58a8b925734439
Juan Sanchez <juan.sanchez1@sscinc.com>
2024-07-10 14:24:34 -0400

    update Education component

--- src/components/Sections/Education/Education.jsx
+++ src/components/Sections/Education/Education.jsx
@@ -1,38 +1,41 @@
-import './Education.css'
-import education from '../../../data/Education.json'
-import { formatDate } from '../../../utils'
+import "./Education.css";
+import education from "../../../data/Education.json";
+import {formatDate} from "../../../utils";
+
 
 function EducationFactory() {
-  return education.map((school, index) => {
-    return (
-      school.meta.display && (
-        <div key={index} className='school'>
-          <div className='row w-100'>
-            <div className='date col-3'>{`${formatDate(
-              school.dates.start
-            )} to ${formatDate(school.dates.end)}`}</div>
-            <div className='title col-9'>
-              <span className='blue-span'></span>
-              {school.degree.short_name} / {school.school.name}{' '}
-              <span className='float-right'>{school.school.location}</span>
-            </div>
-          </div>
-          <div className='responsibilities row w-100'>
-            <p className='col'>{school.degree.long_name}</p>
+  return education.map((edu, index) => {
+      let outerClass = "row w-100";
+      let innerClass = "title col-9";
+      let dateClass = "date col-3";
+      let responsibilitiesClass = "responsibilities row w-100";
+
+      dateClass = !edu.dates.showDate ? "hidden " + dateClass : dateClass;
+      innerClass = !edu.dates.showDate ? "title col-12" : innerClass;
+
+      return (
+      edu.meta.display && <div key={index} className="school">
+        <div className={outerClass}>
+          <div className={dateClass}>{`${formatDate(edu.dates.start)} to ${formatDate(edu.dates.end)}`}</div>
+          <div className={innerClass}>
+            <span className="blue-span"></span>{edu.degree.short_name} / {edu.school.name} <span className="float-right">{edu.school.location}</span>
           </div>
         </div>
-      )
+        <div className={responsibilitiesClass}>
+          <p className="col">{edu.degree.long_name}</p>
+        </div>
+      </div>
     )
   })
 }
 
-export default function Education() {
+export default function Education(){
   return (
-    education.length > 0 && (
-      <div id='education' className='section'>
-        <h2 className='row'>Education</h2>
-        {EducationFactory()}
-      </div>
-    )
+      education.length > 0 && (
+          <div id="education" className="section">
+            <h2 className="row">Education</h2>
+            { EducationFactory() }
+          </div>
+      )
   )
 }
