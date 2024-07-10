1cdf71e8105894b7900272517b88953cb9464e51
Juan Sanchez <juan.sanchez1@sscinc.com>
2024-07-10 14:20:11 -0400

    update example data

--- src/components/Sections/Achievements/Achievements.jsx
+++ src/components/Sections/Achievements/Achievements.jsx
@@ -2,28 +2,26 @@ import './Achievements.css'
 import achievements from '../../../data/Achievements.json'
 
 const AchievementsFactory = () => {
-  return achievements.map((award, index) => {
-    return (
-      award.meta.display && (
-        <div key={index} className='row separator'>
+    return achievements.map((award, index) => {
+        return (
+            award.meta.display && (
+                <div key={index} className='row separator'>
           <span className='col-4'>
             <strong>{award.name}</strong>
           </span>
-          <span className='col-6'>{award.description}</span>
-          <span className='col-2 blue-span float-right'>{award.year}</span>
-        </div>
-      )
-    )
-  })
+                    <span className='col-6'>{award.description}</span>
+                    <span className='col-2 blue-span float-right'>{award.year}</span>
+                </div>
+            )
+        )
+    })
 }
 
 export default function Achievements() {
-  return (
-    achievements.length > 0 && (
-      <div id='achievements' className='section'>
-        <h2 className='row'>Awards, Achievements, and Certifications</h2>
-        {AchievementsFactory()}
-      </div>
+    return (
+        achievements.length > 0 && (<div id="achievements" className="section">
+        <h2 className="row">Awards, Achievements, and Certifications</h2>
+        { AchievementsFactory() }
+      </div>)
     )
-  )
 }
